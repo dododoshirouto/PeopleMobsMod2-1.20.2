0636c2c3aa2dc255d32740fe5ce208443993307a
dododoshirouto <info@dodoneko.site>
2024-02-20 13:59:11 +0900

    move all function to PMM2_HumanoidRenderer

--- src/main/java/site/dodoneko/peoplemobsmod2/base/PMM2_HumanoidMobRenderer.java
+++ src/main/java/site/dodoneko/peoplemobsmod2/base/PMM2_HumanoidMobRenderer.java
@@ -3,10 +3,7 @@ package site.dodoneko.peoplemobsmod2.base;
 import java.util.Map;
 
 import com.google.common.collect.Maps;
-import com.mojang.blaze3d.vertex.PoseStack;
-
 import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.layers.CustomHeadLayer;
@@ -27,39 +24,26 @@ public abstract class PMM2_HumanoidMobRenderer<T extends Mob, M extends Humanoid
     public static final ResourceLocation TEXTURE_DEFAULT = new ResourceLocation(
             "textures/entity/" + "sample-chan" + ".png");
 
-    public PMM2_HumanoidMobRenderer(EntityRendererProvider.Context entity, M model, float shadowRadius) {
-        this(entity, model, shadowRadius, 1.0F, 1.0F, 1.0F);
+    public float modelScale = 0.8F;
+
+    @SuppressWarnings("null")
+    public PMM2_HumanoidMobRenderer(EntityRendererProvider.Context entity, M model, float modelScale) {
+        super(entity, model, modelScale/2);
         PeopleMobsMod2.LOGGER.info(
                 "[PMM2] PMM2_HumanoidMobRenderer(EntityRendererProvider.Context entity, M model, float shadowRadius)");
-    }
-
-    /** with Armor and Item layers. */
-    @SuppressWarnings("null")
-    public PMM2_HumanoidMobRenderer(EntityRendererProvider.Context entity, M model1, M model2, M model3,
-            float shadowRadius) {
-        this(entity, model1, shadowRadius);
-        PeopleMobsMod2.LOGGER.info(
-                "[PMM2] PMM2_HumanoidMobRenderer(EntityRendererProvider.Context entity, M model1, M model2, M model3, float shadowRadius)");
-        this.addLayer(new HumanoidArmorLayer<>(this, model2, model3, entity.getModelManager()));
-    }
-
-    @SuppressWarnings("null")
-    public PMM2_HumanoidMobRenderer(EntityRendererProvider.Context entity, M model1, M model2, M model3) {
-        this(entity, model1, model2, model3, 0.5F);
-        PeopleMobsMod2.LOGGER
-                .info("PMM2_HumanoidMobRenderer(EntityRendererProvider.Context entity, M model1, M model2, M model3)");
-    }
-
-    @SuppressWarnings("null")
-    public PMM2_HumanoidMobRenderer(EntityRendererProvider.Context entity, M model, float shadowRadius, float scaleX,
-            float scaleY, float scaleZ) {
-        super(entity, model, shadowRadius);
-        PeopleMobsMod2.LOGGER.info(
-                "[PMM2] PMM2_HumanoidMobRenderer(EntityRendererProvider.Context entity, M model, float shadowRadius, float scaleX, float scaleY, float scaleZ)");
-        this.addLayer(new CustomHeadLayer<>(this, entity.getModelSet(), scaleX, scaleY, scaleZ,
+        this.modelScale = modelScale;
+        
+        this.addLayer(new CustomHeadLayer<>(this, entity.getModelSet(), modelScale, modelScale, modelScale,
                 entity.getItemInHandRenderer()));
         this.addLayer(new ElytraLayer<>(this, entity.getModelSet()));
         this.addLayer(new ItemInHandLayer<>(this, entity.getItemInHandRenderer()));
+    }
+
+    @SuppressWarnings("null")
+    public void addArmorLayers(EntityRendererProvider.Context entity, M innerArmorModel, M outerArmorModel) {
+        PeopleMobsMod2.LOGGER.info(
+                "[PMM2] addArmorLayers(EntityRendererProvider.Context entity, M innerArmorModel, M outerArmorModel)");
+        this.addLayer(new HumanoidArmorLayer<>(this, innerArmorModel, outerArmorModel, entity.getModelManager()));
     }
 
     public static void addTexture(Class<?> entityClass, String texturePath) {
@@ -90,15 +74,5 @@ public abstract class PMM2_HumanoidMobRenderer<T extends Mob, M extends Humanoid
             return super.isShaking(entity) || ((Zombie) entity).isUnderWaterConverting();
 
         return super.isShaking(entity);
-    }
-
-    // DEBUG
-
-    @Override
-    public void render(T p_114485_, float p_114486_, float p_114487_, PoseStack p_114488_, MultiBufferSource p_114489_,
-            int p_114490_) {
-        PeopleMobsMod2.LOGGER.info("[PMM2] PMM2_HumanoidMobRenderer.render()", p_114485_.toString(), p_114486_,
-                p_114487_, p_114488_, p_114489_, p_114490_);
-        super.render(p_114485_, p_114486_, p_114487_, p_114488_, p_114489_, p_114490_);
     }
 }
--- src/main/java/site/dodoneko/peoplemobsmod2/renderer/PMM2_ZombieRenderer.java
+++ src/main/java/site/dodoneko/peoplemobsmod2/renderer/PMM2_ZombieRenderer.java
@@ -1,7 +1,5 @@
 package site.dodoneko.peoplemobsmod2.renderer;
 
-import net.minecraft.client.model.ZombieModel;
-import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.ZombieRenderer;
@@ -10,20 +8,17 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import site.dodoneko.peoplemobsmod2.PeopleMobsMod2;
 import site.dodoneko.peoplemobsmod2.base.PMM2_HumanoidMobRenderer;
+import site.dodoneko.peoplemobsmod2.model.PMM2_ZombieModel;
 
 @OnlyIn(Dist.CLIENT)
-public class PMM2_ZombieRenderer<T extends Zombie> extends PMM2_HumanoidMobRenderer<T, ZombieModel<T>> {
+public class PMM2_ZombieRenderer<T extends Zombie> extends PMM2_HumanoidMobRenderer<T, PMM2_ZombieModel<T>> {
 
     ZombieRenderer ref;
 
-    public PMM2_ZombieRenderer(EntityRendererProvider.Context entity) {
-        this(entity, ModelLayers.ZOMBIE, ModelLayers.ZOMBIE_INNER_ARMOR, ModelLayers.ZOMBIE_OUTER_ARMOR);
-        PeopleMobsMod2.LOGGER.info("[PMM2] PMM2_ZombieRenderer(EntityRendererProvider.Context entity)");
-    }
-
     @SuppressWarnings("null")
-    public PMM2_ZombieRenderer(EntityRendererProvider.Context entity, ModelLayerLocation modelLayer_1, ModelLayerLocation modelLayer_2, ModelLayerLocation modelLayer_3) {
-        super(entity, new ZombieModel<>(entity.bakeLayer(modelLayer_1)), new ZombieModel<>(entity.bakeLayer(modelLayer_2)), new ZombieModel<>(entity.bakeLayer(modelLayer_3)), 0.5F);
-        PeopleMobsMod2.LOGGER.info("[PMM2] PMM2_ZombieRenderer(EntityRendererProvider.Context entity, ModelLayerLocation modelLayer_1, ModelLayerLocation modelLayer_2, ModelLayerLocation modelLayer_3)");
+    public PMM2_ZombieRenderer(EntityRendererProvider.Context entity) {
+        super(entity, new PMM2_ZombieModel<>(entity.bakeLayer(ModelLayers.ZOMBIE)), 0.8F);
+        PeopleMobsMod2.LOGGER.info("[PMM2] PMM2_ZombieRenderer(EntityRendererProvider.Context entity)");
+        this.addArmorLayers(entity, new PMM2_ZombieModel<>(entity.bakeLayer(ModelLayers.ZOMBIE_INNER_ARMOR)), new PMM2_ZombieModel<>(entity.bakeLayer(ModelLayers.ZOMBIE_OUTER_ARMOR)));
     }
 }
