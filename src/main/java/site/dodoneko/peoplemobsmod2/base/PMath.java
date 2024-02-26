7d522c927d0c4efcb4a01cc8d3a2b6db78dd3c67
dododoshirouto <info@dodoneko.site>
2024-02-26 21:17:53 +0900

    add pow, sqrt, easeIn, easeOut in PMath

--- src/main/java/site/dodoneko/peoplemobsmod2/base/PMath.java
+++ src/main/java/site/dodoneko/peoplemobsmod2/base/PMath.java
@@ -3,46 +3,79 @@ package site.dodoneko.peoplemobsmod2.base;
 public class PMath {
     public static final float Deg2Rad = 0.0174532F;
     public static final float PI = 3.141592653F;
-    public static final float PI2 = PI*2;
+    public static final float PI2 = PI * 2;
 
     public static float toRad(float deg) {
         return deg * Deg2Rad;
     }
 
     public static float sin(float v) {
-        return (float)Math.sin(v);
+        return (float) Math.sin(v);
     }
+
     public static float sinD(float v) {
-        return (float)Math.sin(v * PI2);
+        return (float) Math.sin(v * PI2);
     }
+
     public static float sin1(float v) {
-        return (float)Math.sin(v * Deg2Rad);
+        return (float) Math.sin(v * Deg2Rad);
     }
+
     public static float cos(float v) {
-        return (float)Math.cos(v);
+        return (float) Math.cos(v);
     }
+
     public static float cosD(float v) {
-        return (float)Math.cos(v * Deg2Rad);
+        return (float) Math.cos(v * Deg2Rad);
     }
+
     public static float cos1(float v) {
-        return (float)Math.cos(v * PI2);
+        return (float) Math.cos(v * PI2);
     }
+
     public static float asin(float v) {
-        return (float)Math.asin(v);
+        return (float) Math.asin(v);
     }
 
     public static float abs(float v) {
-        return (float)Math.abs(v);
+        return (float) Math.abs(v);
     }
+
     public static float max(float a, float b) {
         return Math.max(a, b);
     }
+
     public static float min(float a, float b) {
         return Math.min(a, b);
     }
+
     public static float clamp(float v, float a, float b) {
         float min = min(a, b);
         float max = max(a, b);
         return max(min, min(max, v));
+    }
+
+    public static float sqrt(float v) {
+        return (float) Math.sqrt(v);
+    }
+
+    public static float pow(float v, float p) {
+        return (float) Math.pow(v, p);
+    }
+
+    public static float pow2(float v) {
+        return pow(v, 2);
+    }
+
+    public static float easeOut(float time) {
+        return sqrt(sin(time * PI / 2));
+    }
+
+    public static float easeIn(float time) {
+        return 1 - sqrt(sin((1 - time) * PI / 2));
+    }
+
+    public static float easeInOut(float time) {
+        return pow2(sin(time * PI / 2));
     }
 }
